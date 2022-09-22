cc53f38202ad527cb9972a468611791faa4d6279
Weslley C <weslleycxs@gmail.com>
2022-09-22 11:29:59 -0300

    Parte das mudanças, até o item 4

--- res/js/default.js
+++ res/js/default.js
@@ -1,3 +1,27 @@
 var Default = {
-	
+
+	alert1: function(){
+
+		Alerts.ok('Quais podem ser os modelos de negócios da Editora Cidade Nova do futuro?', {
+			description: "Com esse desafio, buscamos propostas de soluções que enderecem as expectativas do novo modelo de negócios que buscamos. Temos a expectativa de receber propostas inovadoras de produtos, serviços, soluções e modelos de negócios que sejam mais dinâmicos e mais adequados ao mercado editorial em constante mutação. Portanto, para estarmos antenados às mudanças nos hábitos de leitura e hábitos de consumo que mudam rapidamente, estamos abertos a receber propostas de outras áreas relacionadas a publicação de conteúdo gratuitos e exclusivos para assinantes, conforme expectativas do novo modelo de negócios da Editora que compartilhamos com os participantes no Hotsite da Hackaton da Editora Cidade Nova."
+		});
+
+	},
+
+	alert2: function(){
+
+		Alerts.ok('Quais podem ser as soluções de Portal de Conteúdo e Loja On-line da Editora Cidade Nova do futuro?', {
+			description: "Com esse desafio, buscamos soluções inovadoras e práticas para administrar, com usabilidade e responsividade, que possam ser utilizadas de forma ágil e prazerosa pelos potenciais clientes e consumidores do conteúdo relevante, que entendemos ter para contribuir para mudar a vida das pessoas, nos diversos dispositivos, incluindo os celulares. Por isso, temos a expectativa de receber propostas inovadoras de Portal de conteúdo, com painel de administração prático e otimizado, com possibilidade de oferecer conteúdo gratuito e conteúdo apenas para diferentes tipos de assinantes, com área exclusiva para os assinantes e área exclusiva para os visitantes gratuitos, desde que se cadastrem no Portal. Estamos abertos para propostas que possibilitem interação de vários tipos de mídia integradas, para ampliar as formas de acesso às diferentes visões do conteúdo apresentado. Temos a expectativa também que esse portal tenha uma loja online própria (B2C), e que tenha adicionalmente uma área exclusiva para colaboradores e parceiros para acesso aos colaboradores e parceiros (B2B), fornecendo ferramentas de gestão para a nossa rede."
+		});
+
+	},
+
+	alert3: function(){
+
+		Alerts.ok('Quais podem ser as soluções de Comunicação e Marketing da Editora Cidade Nova do futuro?', {
+			description: "Com esse desafio, buscamos soluções inovadoras para melhorar e ampliar abrangência e impacto dos nossos conteúdos na sociedade como um todo, aproveitando as oportunidades de tráfego orgânico e a atuação de forma integrada nas redes sociais, canais da Editora e parceiros, para termos uma estratégia de comunicação e marketing mais eficaz e alinhada com os desafios do mercado cada vez mais digital. Nosso objetivo principal é disponibilizar nossos conteúdos gratuitos e exclusivos para assinantes, para chegar ao maior número de pessoas possível. Buscamos encontrar soluções que ampliem o máximo de cadastro dos visitantes em nosso portal, a realização de novas assinaturas de conteúdo, a conversão em aumento de vendas na nossa loja on-line etc. Ou seja, soluções que otimizem a comunicação com os apreciadores do conteúdo que produzimos, e que ajudem a ampliar cada vez mais o alcance na sociedade."
+		});
+
+	}
+
 }
