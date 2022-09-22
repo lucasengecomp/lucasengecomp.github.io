95b1bf986443d9e8cb3a1a16a4dda42cfef6e802
Lucas Castro <lucasengecomp@gmail.com>
2022-09-22 17:19:47 -0300

    Alertas da perguntas formatados

--- res/js/default.js
+++ res/js/default.js
@@ -3,7 +3,7 @@ var Default = {
 	alert1: function(){
 
 		Alerts.ok('Quais podem ser os modelos de negócios da Editora Cidade Nova do futuro?', {
-			description: "Com esse desafio, buscamos propostas de soluções que enderecem as expectativas do novo modelo de negócios que buscamos. Temos a expectativa de receber propostas inovadoras de produtos, serviços, soluções e modelos de negócios que sejam mais dinâmicos e mais adequados ao mercado editorial em constante mutação. Portanto, para estarmos antenados às mudanças nos hábitos de leitura e hábitos de consumo que mudam rapidamente, estamos abertos a receber propostas de outras áreas relacionadas a publicação de conteúdo gratuitos e exclusivos para assinantes, conforme expectativas do novo modelo de negócios da Editora que compartilhamos com os participantes no Hotsite da Hackaton da Editora Cidade Nova."
+			description: "<p>Propostas de produtos e serviços que se apliquem a modelos de negócios mais dinâmicos e mais adequados ao mercado editorial em constante mutação. Portanto, é preciso estar atento às mudanças nos hábitos de leitura e de consumo, tendo em vista os recursos tecnológicos atuais e a infraestrutura da própria Editora.</p><p><b>Algumas indicações:</b> novas formas de publicação de conteúdos gratuitos e áreas exclusivas para assinantes.</p>"
 		});
 
 	},
@@ -11,7 +11,7 @@ var Default = {
 	alert2: function(){
 
 		Alerts.ok('Quais podem ser as soluções de Portal de Conteúdo e Loja On-line da Editora Cidade Nova do futuro?', {
-			description: "Com esse desafio, buscamos soluções inovadoras e práticas para administrar, com usabilidade e responsividade, que possam ser utilizadas de forma ágil e prazerosa pelos potenciais clientes e consumidores do conteúdo relevante, que entendemos ter para contribuir para mudar a vida das pessoas, nos diversos dispositivos, incluindo os celulares. Por isso, temos a expectativa de receber propostas inovadoras de Portal de conteúdo, com painel de administração prático e otimizado, com possibilidade de oferecer conteúdo gratuito e conteúdo apenas para diferentes tipos de assinantes, com área exclusiva para os assinantes e área exclusiva para os visitantes gratuitos, desde que se cadastrem no Portal. Estamos abertos para propostas que possibilitem interação de vários tipos de mídia integradas, para ampliar as formas de acesso às diferentes visões do conteúdo apresentado. Temos a expectativa também que esse portal tenha uma loja online própria (B2C), e que tenha adicionalmente uma área exclusiva para colaboradores e parceiros para acesso aos colaboradores e parceiros (B2B), fornecendo ferramentas de gestão para a nossa rede."
+			description: "<p>Soluções que apresentem gerenciamento das informações dos painéis administrativos de forma mais prática e intuitiva. Para o portal de conteúdo e loja On-line, propiciar uma experiência mais agradável de navegação aos usuários (clientes, consumidores e assinantes), com usabilidade e responsividade para os diferentes dispositivos, incluindo os celulares.</p><p><b>Algumas indicações:</b> painéis administrativos com recursos práticos otimizados para todos os tipos de alterações, inclusive em lote; portal com possibilidade de oferecer conteúdo gratuito e conteúdo apenas para assinantes de diferentes perfis e produtos. Loja on-line integrada ao portal (B2C), que tenha uma área exclusiva para acesso dos colaboradores e parceiros (B2B), fornecendo ferramentas de gestão para toda a nossa rede.</p>"
 		});
 
 	},
@@ -19,7 +19,7 @@ var Default = {
 	alert3: function(){
 
 		Alerts.ok('Quais podem ser as soluções de Comunicação e Marketing da Editora Cidade Nova do futuro?', {
-			description: "Com esse desafio, buscamos soluções inovadoras para melhorar e ampliar abrangência e impacto dos nossos conteúdos na sociedade como um todo, aproveitando as oportunidades de tráfego orgânico e a atuação de forma integrada nas redes sociais, canais da Editora e parceiros, para termos uma estratégia de comunicação e marketing mais eficaz e alinhada com os desafios do mercado cada vez mais digital. Nosso objetivo principal é disponibilizar nossos conteúdos gratuitos e exclusivos para assinantes, para chegar ao maior número de pessoas possível. Buscamos encontrar soluções que ampliem o máximo de cadastro dos visitantes em nosso portal, a realização de novas assinaturas de conteúdo, a conversão em aumento de vendas na nossa loja on-line etc. Ou seja, soluções que otimizem a comunicação com os apreciadores do conteúdo que produzimos, e que ajudem a ampliar cada vez mais o alcance na sociedade."
+			description: "<p>Soluções para melhorar e ampliar o impacto dos nossos conteúdos na sociedade, aproveitando as oportunidades de tráfego orgânico e a atuação de forma integrada nas redes sociais, canais da Editora e parceiros, para termos uma estratégia de comunicação e marketing mais eficaz.</p><p><b>Algumas Indicações:</b> propostas que favoreçam a divulgação dos nossos conteúdos gratuitos e exclusivos para assinantes, fazendo-os chegar a novos públicos. Buscamos encontrar soluções que ampliem: o cadastro de visitantes em nosso portal, a realização de novas assinaturas, a conversão de vendas na loja on-line etc.</p>"
 		});
 
 	}
